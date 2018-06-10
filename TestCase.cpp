984a50ddabd1a4af094a66d2320b5c8587330199
avital477 <33714153+avital477@users.noreply.github.com>
2018-06-10 12:13:38 +0300

    Add files via upload

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,6 +1,2 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-#include "TestCase.hpp"
-using namespace std;
+
 
--- TestCase.hpp
+++ TestCase.hpp
@@ -1,42 +1,77 @@
+#pragma once
 #include <iostream>
 #include <sstream>
 #include <string>
 using namespace std;
 
 int numOfFunction = 0;
+int Pass=0;
+int NoPass=0;
 
-class TestCase(){
-
-    TestCase(string TestName, ostream& os){
+class TestCase{
+    public:
+    string myname;
+    ostream& os;
+    
+    TestCase(string TestName, ostream& os):os(os){
         numOfFunction = 0;
+        Pass=0;
+        NoPass=0;
+        myname= TestName;
     }
-    template <typename T, typename U>{
-        void check_equal(T a, T b){
-            numOfFunction++;
-            if(a != b)
-                print();   
-        }
-        void check_different(T a, T b){
-            numOfFunction++;
-            if(a == b)
-                print(); 
-        }
-        void check_output(T os, string s){
-            numOfFunction++;
-            os << s;
-            string os_str =  os.str();
-            if(os_str != s)
-                print();
-        }
-        //void check_function(T ) 
-        void print(){
-          cout<< TestName <<": Failure in test #" << numOfFunction << ": Function should return "<< 
-
-
-        }
+    
+    template <typename T> TestCase& check_equal(T a, T b){
+        numOfFunction++;
+        if(a == b)
+            Pass++;
+        else{
+             NoPass++;
+            os << myname <<": Failure in test #" << numOfFunction <<": " << a <<" should equal" << b<<"!"<<endl;     
+         }
+    return *this;     
     }
 
+    template <typename T> TestCase& check_different(T a, T b){
+        numOfFunction++;
+        if(a == b){
+            NoPass++;
+            os << myname <<": Failure in test #" << numOfFunction <<": " << a <<" should not equal" << b<<"!"<< endl; 
+        }
+        else
+            Pass++;
+        return *this;      
+    }
+    template <typename T> TestCase& check_output(T variable, string s){
+        numOfFunction++;
+        stringstream p;
+        p << variable;   // תכניס את os לp- תהפוך אותו ךstring
+        if(p.str() != s){
+            NoPass++;
+            
+            os << myname <<": Failure in test #" << numOfFunction <<": string value should be "<< s <<" but is " << variable << endl;
+        }
+        else
+            Pass++;
 
+        return *this;  
+    }
 
+    template <typename Function, typename T, typename Ans> TestCase& check_function(Function f, T a , Ans ans ) {
+        numOfFunction++;
+        if(f(a) == ans){
+            Pass++;
+        }
+        else{
+            NoPass++;
+        os << myname <<": Failure in test #" << numOfFunction <<": Function should return "<< ans << " but returned "<<f(a)<<"!" << endl;
+        }
+     return *this;  
+    }
+    TestCase& print(){
+    cout << myname <<": " << NoPass << " failed, " << Pass<< " passed, " << numOfFunction << " total." << endl ;
+    cout << "---" << endl; 
+    return *this;  
+    }
 
-}
+};
+
